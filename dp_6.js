5c3b7995aa164c12636a26283905eb18e29c93e1
HarshanAruchami <harshanaruchami@gmail.com>
2023-03-15 10:19:55 +0530

    yet to

--- dp_6.js
+++ dp_6.js
@@ -1,5 +1,4 @@
 class Product {
-<<<<<<< HEAD
     constructor(productcode, productname, stock, price) {
         this.productcode = productcode;
         this.productname = productname;
@@ -53,48 +52,3 @@ class Activity extends Product {
 let stock = new Activity(110, "GIFT-BOX", 50, 10, "RAM", 40);
 stock.display();
 stock.sales();
-=======
-  constructor(productcode, productname, stock, price) {
-    this.productcode = productcode;
-    this.productname = productname;
-    this.stock = stock;
-    this.price = price;
-  }
-  get getstock() {
-    return this.stock;
-  }
-  display() {
-    console.log(
-      `Productcode : ${this.productcode} \nProductname : ${this.productname}\nStock : ${this.stock}\nPrice : ${this.price}`
-    );
-  }
-}
-class Activity extends Product {
-  constructor(
-    productcode,
-    productname,
-    stock,
-    price,
-    customername,
-    quantitysold,
-    totalamount,
-    Neededstock
-  ) {
-    super(productcode, productname, stock, price);
-    this.customername = customername;
-    this.quantitysold = quantitysold;
-    this.totalamount = totalamount;
-    this.Neededstock = Neededstock;
-  }
-  sales() {
-    if (this.getstock == 0) {
-      console.log("NO STOCKS LEFT :( " + this.customername);
-    } else {
-      console.log("WE HAVE THE REQUIRED STOCK " + this.customername);
-    }
-  }
-}
-let stock = new Activity(110, "GIFT-BOX", 0, 10, "RAM");
-stock.display();
-stock.sales();
->>>>>>> c481e2ab39238029b4700239cd16d28f7f00a9a0
