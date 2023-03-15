c481e2ab39238029b4700239cd16d28f7f00a9a0
HarshanAruchami <harshanaruchami@gmail.com>
2023-03-15 07:13:48 +0530

    yet to complete

--- dp_6.js
+++ dp_6.js
@@ -1,30 +1,44 @@
 class Product {
-    constructor(productcode, productname, stock, price) {
-        this.productcode = productcode;
-        this.productname = productname;
-        this.stock = stock;
-        this.price = price;
-
-
-    }
-    display() {
-        document.getElementById("output").innerHTML = ("Productcode : " + this.productcode + "<br>" + "Productname : " + this.productname + "<br>" + "Stock : " + this.stock + "<br>" + "Price : " + this.price);
-    }
+  constructor(productcode, productname, stock, price) {
+    this.productcode = productcode;
+    this.productname = productname;
+    this.stock = stock;
+    this.price = price;
+  }
+  get getstock() {
+    return this.stock;
+  }
+  display() {
+    console.log(
+      `Productcode : ${this.productcode} \nProductname : ${this.productname}\nStock : ${this.stock}\nPrice : ${this.price}`
+    );
+  }
 }
 class Activity extends Product {
-    constructor(productcode, productname, stock, price, customername, quantitysold, totalamount) {
-        super(productcode, productname, stock, price);
-        this.customername = customername;
-        this.quantitysold = quantitysold;
-        this.totalamount = totalamount;
-
+  constructor(
+    productcode,
+    productname,
+    stock,
+    price,
+    customername,
+    quantitysold,
+    totalamount,
+    Neededstock
+  ) {
+    super(productcode, productname, stock, price);
+    this.customername = customername;
+    this.quantitysold = quantitysold;
+    this.totalamount = totalamount;
+    this.Neededstock = Neededstock;
+  }
+  sales() {
+    if (this.getstock == 0) {
+      console.log("NO STOCKS LEFT :( " + this.customername);
+    } else {
+      console.log("WE HAVE THE REQUIRED STOCK " + this.customername);
     }
-    sales(){
-
-    }
-    print() {
-        document.getElementById("output").innerHTML = ("Productcode : " + this.productcode + "<br>" + "Productname : " + this.productname + "<br>" + "Stock : " + this.stock + "<br>" + "Price : " + this.price + "<br>" + "Customer Name : " + this.customername + "<br>" + "Product Code :"+ this.productcode+"<br>"+"Quantity Sold :");
-    }
-
+  }
 }
-
+let stock = new Activity(110, "GIFT-BOX", 0, 10, "RAM");
+stock.display();
+stock.sales();
